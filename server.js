bf587d63d8cd880f02424a289ba86e056943a635
varsha <varshaarulkannan@gmail.com>
2025-02-24 14:35:36 +0530

    updated

---
+++ server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.listen(3000, () => console.log("` Server running on http://localhost:3000`"));
+
+app.post("/email", async (req, res) => {
+    try {
+        const newemail = new email(req.body);
+        await newemail.save();
+        res.status(201).json({ message: "email created suscefully", email: newemail });
+    } catch (error) {
+        res.status(400).json({ message: "Error in saving email", error });
+    }
+});
+
+
+app.get("/emails", async (req, res) => {
+    try {
+        const books = await emails.find();
+        res.json(emails);
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching emails", error });
+    }
+});
+
+
